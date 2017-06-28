964110f4c4498ff18f9e8936c5a784c5f84e9dc4
Brandon Semilla <semibran@gmail.com>
2017-06-27 19:54:02 -0700

    argument shifting

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 module.exports = function h(tag, attributes, children) {
+  if (!children && (typeof attributes !== 'object' || Array.isArray(attributes))) {
+    children = attributes
+    attributes = null
+  }
   return [tag, attributes, children]
 }
